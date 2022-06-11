a7673e58ed4a65f9bc12a4e4f141ddb01939565f
Vetreria <90621103+Vetreria@users.noreply.github.com>
2022-06-11 06:49:21 +0300

    json dict

--- main.py
+++ main.py
@@ -1,9 +1,19 @@
-
+import json
 
 def open_file():
+    q_a = {}
     with open("DATA/quiz-questions/1vs1200.txt", "r", encoding="KOI8_R") as file:
         file_contents = file.read()
-    print(file_contents)
+        # for line in file:
+        #     key, *value = line.split(":")
+            
+        #     q_a[key] = value
+        file_contents_splitten = file_contents.split('\n\n')
+        questions = [question for question in file_contents_splitten if 'Вопрос' in question]
+        answers = [answer for answer in file_contents_splitten if 'Ответ' in answer]
+        q_a = dict(zip(questions, answers))
+    with open("DATA/qa.json", 'w', encoding='utf8') as file:
+        json.dump(q_a, file, ensure_ascii=False)
 
 
 
