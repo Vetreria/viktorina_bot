23b4f43a110eb8359dd1b0dd3f78a48a280b252d
Vetreria <90621103+Vetreria@users.noreply.github.com>
2022-06-11 05:38:35 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,23 @@
+
+
+
+
+
+
+
+
+def open_file():
+    with open("\DATA\quiz-questions\1vs1200.txt", "r", encoding="KOI8_R") as file:
+        file_contents = file.read()
+    print(file_contents)
+
+
+
+
+
+def main():
+    open_file
+
+
+if __name__ == "__main__":
+    main()
