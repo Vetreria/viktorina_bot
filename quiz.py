8344ef664093fc5122220ae2d0c8fc7885f00d28
Vetreria <90621103+Vetreria@users.noreply.github.com>
2022-06-19 18:55:25 +0300

    parce all qa

--- quiz.py
+++ quiz.py
@@ -1,5 +1,4 @@
 import os
-import random
 import json
 import re
 
@@ -11,25 +10,34 @@ import re
 
 
 def get_qa(dir_patch):
-    with open(f'{dir_patch}/{random.choice(os.listdir(dir_patch))}', "r", encoding="KOI8_R") as file:
-        quiz_qa = {}
-        file_contents = file.read()
-        file_contents_splitten = file_contents.split('\n\n\n')
-        for question in file_contents_splitten:
-            qa_s = question.split('\n\n')
-            for text in qa_s:
-                if re.findall('Вопрос.*:', text):
-                    question_text = re.split('Вопрос.*:', text)[1]
-                    question_text = question_text.replace('\n', ' ')
-                if re.findall('Ответ:', text):
-                    answer_text = re.split('Ответ:', text)[1]
-                    answer_text = answer_text.replace('\n', ' ')
-            quiz_qa[question_text] = answer_text
+    quiz_qa = {}
+    for filename in os.listdir(dir_patch):
+        with open(f'{dir_patch}/{filename}', "r", encoding="KOI8_R") as file:
+            file_contents = file.read()
+            file_contents_splitten = file_contents.split('\n\n\n')
+            for question in file_contents_splitten:
+                qa_s = question.split('\n\n')
+                for text in qa_s:
+                    if re.findall('Вопрос.*:', text):
+                        question_text = re.split('Вопрос.*:', text)[1]
+                        question_text = question_text.replace('\n', ' ')
+                    if re.findall('Ответ:', text):
+                        answer_text = re.split('Ответ:', text)[1]
+                        answer_text = answer_text.replace('\n', ' ')
+                quiz_qa[question_text] = answer_text
 #  quiz_qa
         # answers = [answer for answer in file_contents_splitten if 'Ответ' in answer]
         # qa = dict(zip(questions, answers))
-    # with open(f"DATA/qa.json", 'w', encoding='utf8') as file:
-    #     json.dump(quiz_qa, file, ensure_ascii=False)
-    return quiz_qa
+    with open(f"DATA/qa.json", 'w', encoding='utf8') as file:
+        json.dump(quiz_qa, file, ensure_ascii=False)
+    # return quiz_qa
 
+
+def main():
+    dir_patch = "DATA/quiz-questions"
+    get_qa(dir_patch)
+
+
+if __name__ == '__main__':
+    main()
 
